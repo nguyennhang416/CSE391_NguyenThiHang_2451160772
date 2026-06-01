e24ba1a65cab696d6daf092c249ebd3e25ae6dda
Nguyenn Hang <nguyennhang3003@nnhang3003gmail.g3003gmailcom>
2026-06-01 22:57:04 +0700

    Hoàn thành tier 7

--- session_04_react_basics/solutions/TIER_2_jsx_variables/Simplevariables .jsx
+++ session_04_react_basics/solutions/TIER_2_jsx_variables/Simplevariables .jsx
@@ -1,6 +1,6 @@
 function SimpleVariables() {
   // Thử thách 1: Thông tin cá nhân
-  const ten = "Đặng Nguyên Trường";
+  const ten = "Nguyễn Thị Hằng";
   const tuoi = 21;
   const queQuan = "Hà Nội";
 
